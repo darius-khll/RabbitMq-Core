72601371640a4f104edc68e6030ae947a2e2ec15
Ali Khalili <ali.khll.1994@gmail.com>
2017-06-21 13:45:44 +0430

    send message broker

--- RabbitMq/Program.cs
+++ RabbitMq/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMq
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main() //Send
         {
-            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "ali", Password = "ali", VirtualHost = "/" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
+
     }
 }
