a898ce61df4397ea8f8e2633bf58184ec471559b
Ali Khalili <ali.khll.1994@gmail.com>
2017-06-22 13:09:23 +0430

    worker queue

---
+++ RabbitMq/WorkerQueue.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq
+{
+    public class WorkerQueue
+    {
+        public void Send(string data)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "ali", Password = "ali", VirtualHost = "/" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = data;
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+            }
+
+            Console.WriteLine("Message sent");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
