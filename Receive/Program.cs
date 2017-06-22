9665d7328031e3129aae64b5968d870d64467151
Ali Khalili <ali.khll.1994@gmail.com>
2017-06-22 13:10:33 +0430

    receive program enhanced

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -9,31 +9,8 @@ namespace Receive
     {
         static void Main(string[] args) //Receive
         {
-            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "ali", Password = "ali", VirtualHost = "/" };
-
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                     noAck: true,
-                                     consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
+            //new Receive().ConsumerReceiver();
+            new WorkerQueue().ConsumerReceiver();
         }
     }
 }
